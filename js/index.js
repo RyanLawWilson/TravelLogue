8bd48b457a2f4af6a75901d9545027cfb0c14643
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2021-06-30 21:38:02 -0600

    Finalize navbar responsiveness and layout

--- js/index.js
+++ js/index.js
@@ -2,8 +2,24 @@
 $(() => {
     var $navBarContainer = $(".index-nav");
     var $searchButton = $(".search-box");
+    var $navBarCollapse = $(".navbar-collapse");
 
-    var navBarHeight = $navBarContainer.css("height");
-    $searchButton.css("height", navBarHeight);
-    $searchButton.css("width", navBarHeight);
+    window.onscroll = () => {
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            $navBarContainer.css("padding-top", 2);
+            $navBarContainer.css("padding-bottom", 2);
+            $navBarContainer.css("height", 50);
+            $searchButton.css("height", 50);
+            $searchButton.css("width", 55);
+            $navBarCollapse.css("top", 50);
+        } else {
+            $navBarContainer.css("padding-top", 20);
+            $navBarContainer.css("padding-bottom", 20);
+            $navBarContainer.css("height", 100);
+            $searchButton.css("height", 100);
+            $searchButton.css("width", 110);
+            $navBarCollapse.css("top", 75);
+        }
+    }
 });
+
